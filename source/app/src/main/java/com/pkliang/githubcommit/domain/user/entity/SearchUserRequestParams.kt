ffbeebbb8a93150f0476d8bdaedd0ae693304ae7
Liang Song <pkliang@gmail.com>
2020-04-02 21:32:33 +0300

    Fix user search entities

--- source/app/src/main/java/com/pkliang/githubcommit/domain/user/entity/SearchUserRequestParams.kt
+++ source/app/src/main/java/com/pkliang/githubcommit/domain/user/entity/SearchUserRequestParams.kt
@@ -2,6 +2,6 @@ package com.pkliang.githubcommit.domain.user.entity
 
 data class SearchUserRequestParams(
     val queryString: String,
-    val first: Int,
+    val first: Int = 20,
     val after: String? = null
 )
--- source/app/src/main/java/com/pkliang/githubcommit/domain/user/entity/SearchUserResponse.kt
+++ source/app/src/main/java/com/pkliang/githubcommit/domain/user/entity/SearchUserResponse.kt
@@ -2,5 +2,6 @@ package com.pkliang.githubcommit.domain.user.entity
 
 data class SearchUserResponse(
     val users: List<User>?,
+    val hasNextPage: Boolean?,
     val nextPage: String?
 )
